2672c3bdba41ceef53f14bd4e11569a64d760cf7
mariusherlea <mariusherlea@gmail.com>
2023-12-20 13:17:27 +0200

    diferent content on diferent page

--- js/app.js
+++ js/app.js
@@ -1,37 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch data from data.json
-  fetch("../data/data.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Use the data to update the HTML content dynamically
-      const destinationContainer = document.getElementById(
-        "destination-container"
-      );
-      const destinations = data.home;
-      console.log(data);
+const body = document.querySelector("body");
+body.textContent = "";
 
-      const destinationDiv = document.createElement("div");
-      destinationDiv.innerHTML = `
+export async function fetchData() {
+  try {
+    const response = await fetch("../data/data.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Fetch data.json error", error);
+  }
+}
+// fetchData();
+export function addLinkItem(text, url, parent) {
+  const liElement = document.createElement("li");
+  const linkElement = document.createElement("a");
+  linkElement.setAttribute("href", url);
+  linkElement.textContent = text;
+  liElement.appendChild(linkElement);
+  parent.appendChild(liElement);
+}
 
-      <nav>
-        <ul>
-          <li><a href=${data.navData[0].url}>${data.navData[0].text}</a></li>
-          <li><a href=${data.navData[1].url}>${data.navData[1].text}</a></li>
-          <li><a href=${data.navData[2].url}>${data.navData[2].text}</a></li>
-          <li><a href=${data.navData[3].url}>${data.navData[3].text}</a></li>
+export async function nav() {
+  const date = await fetchData();
+  console.log(date);
+  if (date) {
+    const navElement = document.createElement("nav");
+    const ulElement = document.createElement("ul");
 
-        </ul>
-      </nav>
+    body.appendChild(navElement);
+    navElement.appendChild(ulElement);
+    date.navData.forEach((element) => {
+      addLinkItem(element.text, element.url, ulElement);
+    });
+  }
+}
+nav();
 
-      <h5>${destinations.upHeader}</h5>
-            <h1>${destinations.header}</h1>
-           
-            <p>${destinations.paragraph}</p>
-    
-            <a href="${destinations.url}">${destinations.exploreBtn}</a>
-          `;
+export function addElementToPage(parent, element, content) {
+  element = document.createElement(element);
+  element.textContent = content;
+  parent.appendChild(element);
+}
+const date = await fetchData();
 
-      destinationContainer.appendChild(destinationDiv);
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-});
+addElementToPage(body, "h1", date.home.header);
+addElementToPage(body, "h5", date.home.upHeader);
+addElementToPage(body, "p", date.home.paragraph);
+addElementToPage(body, "button", date.home.exploreBtn);
--- js/appDest.js
+++ js/appDest.js
@@ -1,44 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch data from data.json
-  fetch("../data/data.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Use the data to update the HTML content dynamically
-      const destinationContainer = document.getElementById(
-        "destination-container"
-      );
-      const destinations = data.destinations;
-      console.log(data);
+import { nav } from "./app.js";
+import { fetchData } from "./app.js";
+import { addElementToPage } from "./app.js";
 
-      const destinationDiv = document.createElement("div");
-      destinationDiv.innerHTML = `
+function addImgToPage(parent, element, content) {
+  element = document.createElement(element);
+  element.src = content;
+  parent.appendChild(element);
+}
 
-      <nav>
-      <ul>
-        <li><a href=${data.navData[0].url}>${data.navData[0].text}</a></li>
-        <li><a href=${data.navData[1].url}>${data.navData[1].text}</a></li>
-        <li><a href=${data.navData[2].url}>${data.navData[2].text}</a></li>
-        <li><a href=${data.navData[3].url}>${data.navData[3].text}</a></li>
+const body = document.querySelector("body");
 
-      </ul>
-      <ul>
-      <li><a href=${data.destinations[0].url}>${data.destinations[0].name}</a></li>
-      <li><a href=${data.destinations[1].url}>${data.destinations[1].name}</a></li>
-      <li><a href=${data.destinations[2].url}>${data.destinations[2].name}</a></li>
-      <li><a href=${data.destinations[3].url}>${data.destinations[3].name}</a></li>
-
-    </ul>
-    </nav>
-
-            <h2>${destinations[0].name}</h2>
-            <img src="${destinations[0].images.png}" alt="${destinations[0].name}">
-            <p>${destinations[0].description}</p>
-            <p>Distance: ${destinations[0].distance}</p>
-            <p>Travel Time: ${destinations[0].travel}</p>
-            <a href="${destinations[0].url}">Learn more</a>
-          `;
-
-      destinationContainer.appendChild(destinationDiv);
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-});
+async function content() {
+  body.textContent = "";
+  const date = await fetchData();
+  console.log(date);
+  if (date) {
+    addElementToPage(body, "h1", date.destinations[0].name);
+    addElementToPage(body, "h5", date.destinations[0].travel);
+    addElementToPage(body, "h3", date.destinations[0].distance);
+    addElementToPage(body, "p", date.destinations[0].description);
+    addImgToPage(body, "img", date.destinations[0].images.png);
+  }
+}
+nav();
+content();
