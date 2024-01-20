a66a93e71b3da366a4bfe08a08b25fe35aadbe99
mariusherlea <mariusherlea@gmail.com>
2024-01-20 15:10:01 +0200

    retrieve an object from an array

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [destinations, setDestinations] = useState([]);
+  const [home, setHome] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("http://localhost:3000/data.json");
         const data = await res.json();
-        console.log(data.destinations);
+        // console.log(data.destinations);
+        // console.log(data.home);
+        setHome(data.home);
         setDestinations(data.destinations);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -22,10 +25,11 @@ function App() {
 
   return (
     <div>
-      {/* Render each destination */}
+      <h1>{home.header}</h1>
       {destinations.map((destination, index) => (
         <div key={index}>
           <h2>{destination.name}</h2>
+
           {/* <img src={destination.images.png} alt={`${destination.name} Image`} /> */}
           <p>{destination.description}</p>
           <p>Distance: {destination.distance}</p>
